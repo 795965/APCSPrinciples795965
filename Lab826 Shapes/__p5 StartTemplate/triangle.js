232959e5023eb5780ccc5e7b1c8d3694e5276d5b
795965 <42558488+795965@users.noreply.github.com>
2019-08-27 08:39:13 -0700

    working on shapes

--- Lab826 Shapes/__p5 StartTemplate/triangle.js
+++ Lab826 Shapes/__p5 StartTemplate/triangle.js
@@ -1,8 +1,11 @@
 class Triangle {
   constructor(x, y, dx, dy){
-    this.x = x;
-    this.y = y;
-    this.z = z;
+    this.x1 = x1;
+    this.y1 = y1;
+    // this.x2 = x2;
+    // this.y2 = y2;
+    // this.x2 = x2;
+    // this.y2 = y2;
     this.dx = dx;
     this.dy = dy;
     this.clr = color(random(255), random(255), random(255));
@@ -16,28 +19,35 @@ class Triangle {
   }
 
   checkedges(){
-    if(this.x < 0){
+    if(this.x1 < 0 || this.x2 < 0 || this.x3 < 0){
       this.dx = -this.dx;
     }
-    if(this.x > width){
+    if(this.x1 > width || this.x2 > width || this.x3 > width){
       this.dx = -this.dx;
     }
-    if(this.y < 0){
+    if(this.y1 < 0 || this.y2 < 0 || this.y3 < 0){
       this.dy = -this.dy;
     }
-    if(this.y > height){
+    if(this.y1 > height || this.y3 > height || this.y3 > height){
       this.dy = -this.dy;
     }
 
   }
   update(){
-    this.x = this.x + this.dx;
+
+
+    this.x1 = this.x1 + this.dx;
+    this.x2 = this.x2 + this.dx;
+    this.x3 = this.x3 + this.dx;
+
+    if()
     this.y = this.y +this.dy;
 
   }
 
   render(){
     fill(this.clr);
-    triangle(this.x, this.y, this.dx, this.dy);
+    triangle(this.x1, this.y1, this.x1+30, this.y1, this.x1+15, this.y1-30);
+
   }
 }
