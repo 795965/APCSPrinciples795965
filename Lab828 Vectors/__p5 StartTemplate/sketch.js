6b242e365dab03f0f17c7a7bfb3c00c06eb61fcd
795965 <42558488+795965@users.noreply.github.com>
2019-08-28 08:43:59 -0700

    lab 828

---
+++ Lab828 Vectors/__p5 StartTemplate/sketch.js
@@ -0,0 +1,33 @@
+//Ziggy Sheynin
+//Lab 828 Vectors
+//  This is a comment
+//  The setup function function is called once when your program begins
+var balls = []; //declares array
+
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+
+  loadBalls(600); //initializes array
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() { //what shows up on console
+background(5,5,5);
+  runBalls();
+
+}//end draw
+
+function loadBalls(x,y,z){ //loads balls into array
+  for(var i = 0; i < x; i++){
+    balls[i]=new Ball(random(width), random(height), random (-2,2), random(-2,2));
+  }//end for
+}
+
+function runBalls(){ //called from draw function
+  for(var i = 0; i < balls.length; i++){
+    balls[i].run();
+}//end for
+}//end runBalls
