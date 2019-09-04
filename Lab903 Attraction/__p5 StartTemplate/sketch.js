0e2458b5339c5e7d5665eadf1490fb37a617a2ad
Zoggy <Augilita@ziggys-MacBook-Air-2.local>
2019-09-04 08:43:14 -0700

    FInished lab 903

--- Lab903 Attraction/__p5 StartTemplate/sketch.js
+++ Lab903 Attraction/__p5 StartTemplate/sketch.js
@@ -4,33 +4,35 @@
 //  The setup function function is called once when your program begins
 var balls = []; //declares array
 var mainBall;
+var mainBall2;
 
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(5, 5, 5);
+  background(5, 5, 5, 20);
 
-  loadBalls(50); //initializes array
+  loadBalls(50);
 
 }
 
-//  The draw function is called @ 30 fps
-function draw() { //what shows up on console
-background(5,5,5,20);
+
+function draw() {
+background(5,5,5, 20);
   runBalls();
-
-}//end draw
-
-function loadBalls(x){ //loads balls into array
-  mainBall = new Ball(random(width/2), random(height/2), random (-1,1), random(-1,1), -1);
-  for(var i = 0; i < x; i++){
-    balls[i]=new Ball(random(width), random(height), random (-1,1), random(-1,1), i);
-  }//end for
 }
 
-function runBalls(){ //called from draw function
+function loadBalls(x){
+  mainBall = new Ball(random(width/2), random(height/2), random (-.4,.4), random(-.4,.4), -1);
+  mainBall2 = new Ball(random(width/2), random(height/2), random (-.4,.4), random(-.4,.4), 0);
+  for(var i = 0; i < x; i++){
+    balls[i]=new Ball(random(width), random(height), random (-1,1), random(-1,1), i+2);
+  }
+}
+
+function runBalls(){
   mainBall.run();
+  mainBall2.run();
   for(var i = 0; i < balls.length; i++){
     balls[i].run();
-}//end for
-}//end runBalls
+}
+}
