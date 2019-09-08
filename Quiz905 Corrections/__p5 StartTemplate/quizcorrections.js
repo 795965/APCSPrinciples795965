6ad027be6993c2dde3a50ed420cecc49d27df917
795965 <42558488+795965@users.noreply.github.com>
2019-09-08 16:34:31 -0700

    quiz905 corrections

---
+++ Quiz905 Corrections/__p5 StartTemplate/quizcorrections.js
@@ -0,0 +1,24 @@
+//Ziggy Sheynin
+//Quiz 905 Corrections
+
+
+//#1
+var num = 1;
+if(num < 4){
+  console.log("Peace");
+}
+console.log("War");
+
+//prints
+//Peace
+//War
+
+//#2
+for(var i= 14; i > 8; i--){
+  i= i-4;
+  console.log(i);
+}
+
+//prints
+//10
+//5
