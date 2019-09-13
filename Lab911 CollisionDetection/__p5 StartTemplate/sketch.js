6ad53e7149dea2cfab45b617f52030a6584ea5d6
795965 <42558488+795965@users.noreply.github.com>
2019-09-13 08:50:48 -0700

    edits

--- Lab911 CollisionDetection/__p5 StartTemplate/sketch.js
+++ Lab911 CollisionDetection/__p5 StartTemplate/sketch.js
@@ -9,20 +9,20 @@ function setup() {
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadObjects(5);
+  loadObjects(1);
 
 
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  background(5, 5, 5, 20);
+  background(5, 5, 5);
   runObjects();
 }
 function loadObjects(n){
-  paddle = new Paddle(400, 400, 100, 30);
+  paddle = new Paddle(400, 400, 150, 30);
   for(var i = 0; i < n; i++){
-    balls[i] = new Ball(random(width), random(height), 4, 4);
+    balls[i] = new Ball(random(width), random(height), 15, 15);
   }
 }//loads balls into array
 function runObjects(){
