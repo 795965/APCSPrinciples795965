fc0c2cb41df48a392e8943aee4b8d68d7874efcb
795965 <42558488+795965@users.noreply.github.com>
2019-10-15 08:44:12 -0700

    finished long list bubble sort

--- lab1015 bubbleSort/__p5 StartTemplate/sketch.js
+++ lab1015 bubbleSort/__p5 StartTemplate/sketch.js
@@ -1,15 +1,20 @@
 //  Ziggy Sheynin
-// 	Lab 1009 BubbleSort
+// 	Lab 1015 BubbleSort
 //  This is a comment
 //  The setup function function is called once when your program begins
 
-var list = [7, 8, 9, 2, 1,3, 3];
+var list = [];
+
+
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
 
+  for(var x = 0; x < 10; x++){
+    list[x] = Math.floor(random(1, 100));
+  }
 
 }
 
@@ -19,6 +24,8 @@ function draw() {
 }
 
 function bubbleSort(list){
+
+
   for(var j = list.length; j > 0 ; j --){
 for (var i = 0 ; i < list.length; i ++){
 
