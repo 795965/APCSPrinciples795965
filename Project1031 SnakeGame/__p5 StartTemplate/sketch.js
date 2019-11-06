715af4e4d49930019bce4ef3c5054e3843be452f
795965 <42558488+795965@users.noreply.github.com>
2019-11-06 08:50:31 -0800

    snek

--- Project1031 SnakeGame/__p5 StartTemplate/sketch.js
+++ Project1031 SnakeGame/__p5 StartTemplate/sketch.js
@@ -21,7 +21,7 @@ function setup() {
 }//end setup
 
 function draw(){
-  background(104, 212, 61);
+background(104, 212, 61);
   snake.run();
   food.render();
   if (checkTangled === true){
@@ -65,87 +65,8 @@ function runObjects(){
 }//end runObjects
 
 
-function update(){
-  if(keyCode === UP_ARROW){
-    head.position(5,5);
-  }
-}//end update
-// var cubeWidth
-// var food;
-// var direction;
-// var snake = [];
-// var column;
-// var row;
-// var snakeHead;
-// var gameOver;
-//
-// function setup() {
-//   var cnv = createCanvas(800, 800);
-//   cnv.position((windowWidth-width)/2, 30);
-//   background(179, 189, 179);
-//   fill(200, 100, 150);
-//   cubeWidth = 20;
-//   column = width / cubeWidth;
-//   row = height / cubeWidth;
-//   snakeHead = new Snake(column, 600, color(170, 30, 100));
-//   food = new Food(cubeWidth * int(random(0,800/cubeWidth)),cubeWidth * int(random(0,800/cubeWidth)), color(30));
-// }// end set up
-//
-// function loadObjects(n){
-//   for(i = 0; i < n; i++){
-//
-// }//end loadObjects
-//
-// function runObjects(){
-//   snake.run();
-//   for(var i = 0; i < snake.body.length; i++){
-//     snake.body[i].run();
+// function update(){
+//   if(keyCode === UP_ARROW){
+//     head.position(5,5);
 //   }
-//   food.run();
-// }// end runObjects
-//
-// //  The draw function is called @ 30 fps
-// function draw(){
-//   frameRate(15)
-//     if(gameState === 1){
-//       if(keyPressed === true){
-//         gameState = 2
-//       }
-//     }else if (gameState === 2) {
-//       background(5,5,5);
-//       runObjects();
-//     }else if (gameState === 3) {
-//       textAlign(CENTER);
-//       text("Game Over", 400, 400);
-//     }
-//
-// }//end draw
-//
-//   if(gameOver === 1){
-//     fill(200, 30, 30);
-//     textSize(90);
-//     text("Game Over", 100, 100);
-//   }
-//
-//   function getFood(){
-//   //need to compute row,col of snake.loc
-//   //and food.loc,
-//   //return (snake.row === food.row && snake.col === food.col)
-//   }//end getFood
-//
-//   function newGame(){
-//     score = 0;
-//     //create the snake object in a random cell near the middle, create the
-//     //food object, make sure it’s not overlapping the snake.
-//     //Put it somewhere that’s not close to the middle.
-//
-//   }// end newGame
-//
-//   function startNewRound(){
-//
-//   }// end startNewRound
-//
-//   function checkTangled(){
-//     //return snake.tangled();
-//   }//end checkTangled
-// }
+//}//end update
--- Project1031 SnakeGame/__p5 StartTemplate/snake.js
+++ Project1031 SnakeGame/__p5 StartTemplate/snake.js
@@ -9,7 +9,7 @@ class Snake{
     this.vel = createVector(0,0);
     this.w = 30;
     this.clr = c;
-
+    this.body =[];
   }// end constructor
 
   run(){
@@ -19,27 +19,7 @@ class Snake{
 
   update(){
     this.keyPressed();
- //    bodySegments[0].x = this.loc.x;
- //    bodySegments[0].y = this.loc.y;
- //
- //    for(var i = bodySegments.length - 1; i >= 1; i--){
- //        bodySegments[i].x = this.segments[i - 1].x;
- //        bodySegments[i].y = this.segments[i - 1].y;
- //      }
- //
- // this.loc.add(this.vel);
- // this.loc.x = constrain(this.loc.x, 0, width-this.w);
- // this.loc.y = constrain(this.loc.y, 0, width-this.w);
- //
- // //once snake eats food then add a segment and move food location
- // if(this.loc.dist(food.loc) === 0){
- //   bodySegments.push(createVector(0,0));
- //   for(i = 0; i < bodySegments.length; i++){
- //     if(food.loc != bodySegments[i].loc){
- //       food.loc = createVector(width / 2 - Math.floor(Math.random()*16-8)*this.w, height / 2+Math.floor(Math.random()*12-6)*this.w);
- //     }
- //   }
- // }
+
 
   }//end update
 
@@ -79,68 +59,5 @@ class Snake{
     }
 
   }//end keyPressed
-  // constructor(x,y, c){
-  //   this.loc = createVector(x,y);
-  //   this.w = cubeWidth;
-  //   this.clr = c;
-  // }//end constructor
-  //
-  // run(){
-  //   this.render();
-  //   this.update();
-  //   this.checkEdges();
-  // }//end run
-  //
-  // render(){
-  //   fill(this.clr);
-  //   rect(this.loc.x, this.loc.y, this.w, this.w);
-  // }// end render
-  //
-  // update(){
-  //   if(keyCode === RIGHT_ARROW){
-  //     if(direction === 2){
-  //       // gameOver = 1;
-  //     }
-  //     this.loc.x = this.loc.x + this.w;
-  //     direction = 1;
-  //   }
-  //   if(keyCode === LEFT_ARROW){
-  //     if(direction === 1){
-  //       // gameOver = 1;
-  //     }
-  //     this.loc.x = this.loc.x - this.w;
-  //     direction = 2;
-  //   }
-  //   if(keyCode === UP_ARROW){
-  //     if(direction === 4){
-  //       // gameOver = 1;
-  //     }
-  //     this.loc.y = this.loc.y - this.w;
-  //     direction = 3;
-  //   }
-  //   if(keyCode === DOWN_ARROW){
-  //     if(direction === 3){
-  //       // gameOver = 1;
-  //     }
-  //     this.loc.y = this.loc.y + this.w;
-  //     direction = 4;
-  //   }
-  // }//end update
-  //
-  // checkEdges(){
-  //   if(this.loc.x > width - this.w || (this.loc.x < 0) || (this.loc.y > height - this.w) || (this.loc.y < 0)){
-  //     gameOver = 1;
-  //   }
-  // }//end checkEdges
-  //
-  // tangled(){
-  //   //for loop checking each segment in the segment array
-  //   for(i = 0; i < this.bodySegments.length; i++){
-  //     //if stament checking if the locations are equal to each other
-  //     if(this.loc.x == this.bodySegments[i].x && this.loc.y == this.bodySegments[i].y){
-  //       console.log("Game Over");
-  //     }
-  //   }
-  // }//end tangled
 
 } //++++++++++++++++ End Snake
