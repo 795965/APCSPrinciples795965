c438281a59d982eeb5e78ef2826e9237118429b4
795965 <42558488+795965@users.noreply.github.com>
2019-12-09 08:51:16 -0800

    lab 1209

---
+++ Lab1209 Ball Sort/__p5 StartTemplate/balls.js
@@ -0,0 +1,32 @@
+//  Ziggy Sheynin
+// 	Lab 1209 Ball Sort
+//  This is a comment
+//  The setup function function is called once when your program begins
+
+class Ball{
+  constructor(x,y,w,h,clr,red){
+    this.loc=createVector(x,y);
+    this.w=w;
+    this.h=h;
+    this.clr = clr;
+    this.red = red;
+
+  }
+run(){//allows me to call all methods in this class
+  this.render();
+}
+
+render(){ //makes balls show up
+  fill(this.clr);
+  ellipse(this.loc.x,this.loc.y,this.w,this.h);
+}
+
+getRed(){  //creates get-able variable for sketch class
+  //this.loc.x=i*barWidth;
+  return this.red;
+}
+
+set(i){
+  this.x = i*40+20;
+}
+} //+++++++++++++++end Bars
---
+++ Lab1209 Ball Sort/__p5 StartTemplate/sketch.js
@@ -0,0 +1,62 @@
+//  Ziggy Sheynin
+// 	Lab 1209 Ball Sort
+//  This is a comment
+//  The setup function function is called once when your program begins
+var balls=[];
+
+
+//put setup code here
+function setup(){
+  var cnv=createCanvas(800,800);
+  cnv.position((windowWidth-width)/2,30);
+  background(5, 5, 5);
+
+  loadBalls(20); //puts balls into balls array
+  frameRate(10); //slows down the speed
+  for(var i=0; i<balls.length; i++){
+    balls[i].run(); //to print the array
+  }
+}
+
+function draw(){ //calls method at 30 fps
+  bubbleSort();
+  runBalls();
+}
+
+function update(){ //to allow for animation
+  for(var i=0; i<20; i++){
+    balls[i].set(i);
+  //  balls[i].getRed(red);
+  }
+  background(5, 5, 5);
+  runBalls();
+
+}
+
+function runBalls(){
+  for(var i = 0; i < 20; i++){
+    balls[i].run();
+  }
+}
+
+function loadBalls(n){ //puts balls into array
+  for (var x=0; x<n; x++){
+    var red = random(0,255);
+    balls[x]= new Ball(x*40+20,400,30,30, color(red, 0, 0),red);
+  }
+}
+
+function bubbleSort(){ //sorting routine
+    for (var j=0; j<19; j++){
+      if(balls[j].getRed()<balls[j+1].getRed()){ //change to check for red color
+        swap(balls, j, j+1);
+        update();
+      }
+   }
+  }
+
+function swap(list,a,b){ //swapping algorithm
+  var temp=list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
