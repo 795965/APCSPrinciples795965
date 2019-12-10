f35877db13edbf84ddcca75f380c476214afd74e
795965 <795965@seq.org>
2019-12-09 19:15:15 -0800

    fixed

--- Lab1209 Ball Sort/__p5 StartTemplate/balls.js
+++ Lab1209 Ball Sort/__p5 StartTemplate/balls.js
@@ -4,24 +4,22 @@
 //  The setup function function is called once when your program begins
 
 class Ball{
-  constructor(x,y,w,h,a,b,c){
+  constructor(x,y,w,h,red,g,b){
     this.loc=createVector(x,y);
     this.w=w;
     this.h=h;
-    this.a=random(255);
+    this.red=random(255);
+    this.g=0;
     this.b=0;
-    this.c=0;
   }
-run(){
-  //main function in class
-  this.render();
 
-}
 render(){
-  fill(this.a,this.b,this.c);
+  fill(this.red,this.g,this.b);
   ellipse(this.loc.x+20,this.loc.y,this.w,this.h);
 }
+
 set(i){
   this.loc.x=i*40;
 }
-}// +++++ end ball class
+
+}// +++++++++ end ball class
